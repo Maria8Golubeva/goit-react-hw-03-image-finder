e3cddd07496ccdb860d7e4b5931fe1fa69102f8a
Maria Golubeva <mitsunari8ishida@gmail.com>
2021-07-26 15:15:50 +0300

    Upload HW

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Searchbar from "./Components/Searchbar";
+import ImageGallery from "./Components/ImageGallery";
+import ImageGalleryItem from "./Components/ImageGalleryItem";
+import Button from "./Components/Button";
+import Loader from "./Components/Loader/Loader";
+import Modal from "./Components/Modal";
+
+import fetchImages from "./Services/PixabayAPI";
+
+class App extends Component {
+  state = {
+    searchQuery: "",
+    modalShow: false,
+    pageNumber: 1,
+    images: [],
+    modalImageURL: "",
+    loader: false,
+    error: "",
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.getItemsForGallery();
+    }
+  }
+
+  toggleModal = () => {
+    this.setState({ modalShow: !this.state.modalShow });
+  };
+
+  openImage = (id) => {
+    this.toggleModal();
+    const { images } = this.state;
+    const clickImage = images.filter((item) => item.id === Number(id));
+    this.setState({ modalImageURL: clickImage[0].largeImageURL });
+  };
+
+  getSearchQuery = (query) => {
+    if (query === this.state.searchQuery) {
+      return;
+    }
+    this.setState({
+      searchQuery: query,
+      images: [],
+      pageNumber: 1,
+      error: "",
+    });
+  };
+
+  getItemsForGallery = async () => {
+    this.setState({ loader: true });
+
+    const { searchQuery, pageNumber } = this.state;
+    try {
+      const newImages = await fetchImages(searchQuery, pageNumber);
+      if (newImages.length === 0) {
+        this.setState({ error: "empty responce" });
+      }
+
+      this.setState(({ images, pageNumber }) => {
+        return {
+          images: [...images, ...newImages],
+          pageNumber: pageNumber + 1,
+        };
+      });
+    } catch (error) {
+      this.setState({ error: error });
+    }
+
+    this.setState({ loader: false });
+  };
+
+  render() {
+    const { images, modalImageURL, loader, error } = this.state;
+
+    return (
+      <>
+        <Searchbar onSubmit={this.getSearchQuery} />
+        <ImageGallery>
+          {images.map((item) => (
+            <ImageGalleryItem
+              key={item.id}
+              id={item.id}
+              url={item.webformatURL}
+              openImage={this.openImage}
+            ></ImageGalleryItem>
+          ))}
+        </ImageGallery>
+
+        {error && (
+          <p>oops... something went wrong, try again later or change query</p>
+        )}
+
+        {loader && <Loader />}
+
+        {images.length > 0 && !loader && (
+          <Button
+            onClick={this.getItemsForGallery}
+            text={"Load more"}
+            totalElements={images.length}
+          />
+        )}
+
+        {this.state.modalShow && (
+          <Modal url={modalImageURL} modalToggle={this.toggleModal} />
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
